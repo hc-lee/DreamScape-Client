220ff5e5443f4576f7b3b5fcd7ddec12afa593dc
hc-lee <hansol_lee@yahoo.com>
2023-12-22 19:32:58 -0800

    12/22/23 - Add null check to DreamPromptBox submission.

--- src/components/DreamPromptBox.jsx
+++ src/components/DreamPromptBox.jsx
@@ -11,9 +11,15 @@ function DreamPromptBox({onInterpretButtonClick}) {
 
     const handleInterpretClick = () => {
         // Define the API endpoint URL
+        // const apiUrl = '/api/generate_image';
+        // const apiUrl = 'http://54.188.27.139:8080/generate_image';
+        const apiUrl = 'http://localhost:8081/generate_image';
+        if (!dreamText || dreamText.length === 0) {
+            alert('Please enter a dream description.');
+            return;
+        }
+
         setIsLoading(true);
-        const apiUrl = '/api/generate_image';
-        // const localApiUrl = 'http://54.188.27.139:8080/generate_image';
 
         // Create a JSON object with the description box text
         const requestBody = {
